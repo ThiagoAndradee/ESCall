c661af67fb1c56ddf4464b5efb8e3dd7eca3c7f1
Thi <thiago.andradecontato@hotmail.com>
2024-09-13 01:37:42 +0200

    Initial Commit

---
+++ ESCall/ESCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESCallApp.swift
+//  ESCall
+//
+//  Created by Andrade, Thiago on 13.09.24.
+//
+
+import SwiftUI
+
+@main
+struct ESCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
